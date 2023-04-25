d2ee6fc8ed9e47635c64ca85a56a1f51a84ee708
keren0527 <131560621+keren0527@users.noreply.github.com>
2023-04-26 01:21:05 +0200

    all files
    every new or edited file must always be commit

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
- var btn = document.getElementById("btn");
- btn.addEventListener('click', (e) =>{
-    e.preventDefault();
-var one = parseInt(document.getElementById("one").value);
- var two =parseInt(document.getElementById("two").value);
- var sum =one + two;
- console.log(sum)
- })
+//  var btn = document.getElementById("btn");
+//  btn.addEventListener('click', (e) =>{
+    // e.preventDefault();
+// var one = parseInt(document.getElementById("one").value);
+//  var two =parseInt(document.getElementById("two").value);
+//  var sum =one + two;
+//  console.log(sum)
+//  })
+
+//  var myDiv = document.getElementById("root");
+//  var p = document.createElement("p");
+//  p.innerHTML="lorem ipsum";
+//  p.classList.add("two");
+//  myDiv.append(p);
  
+ var myButton = document.getElementById("myButton");
+ var myImage = document.getElementById("myImage");
+ myButton.addEventListener("click",function(){
+    myImage.src = "jerry.jpg";
+ })
